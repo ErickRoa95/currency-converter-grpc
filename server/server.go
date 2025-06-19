44e4350c00696361f9e7206d1fdff5ff25820870
ErickRodriguezDev <erick.rodriguez@wizeline.com>
2025-06-19 11:17:01 -0700

    ADD: Converte for Converter Endpoints

--- server/server.go
+++ server/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"log"
@@ -15,33 +14,22 @@ var(
 	port = flag.Int("port", 50051, "The server port")
 )
 
-type server struct {
-	pb.UnimplementedCurrencyServer
-}
-
-func (s *server) Converter(ctx context.Context, req *pb.ConverterRequest) (*pb.ConverterResponse, error){
-	log.Printf("Received: %v", req)
-	currency := "Mexican pesos"
-	dollar := int32(1)
-	amount:= int32(19)
-	return &pb.ConverterResponse{
-		CurrencyName: &currency,
-		Dollar: &dollar, 
-		Amount: &amount,
-	}, nil
-}
-
 func main (){
 	flag.Parse()
 	addr:= fmt.Sprintf(":%d", *port)
+
+	// Initied listening port for grpc server.
 	list, err:= net.Listen("tcp", addr)
 	if err!=nil {
 		log.Fatalf("Failed to listen %v", err)
 	}
 
+	// Create & register server dependencies.
 	s := grpc.NewServer()
 	pb.RegisterCurrencyServer(s, &server{})
-	log.Printf("server listening at %v", list.Addr())
+
+	// Serve Grpc Server at specific port.
+	log.Printf("GRPC Server listening at %v !", list.Addr())
 	if err:= s.Serve(list); err != nil {
 		log.Fatalf("Failed to serve : %v", err)
 	}
