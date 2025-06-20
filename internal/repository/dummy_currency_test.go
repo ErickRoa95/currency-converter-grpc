0fc720bd68ba179cadd8a02544100f7ce97c8fc9
ErickRodriguezDev <erick.rodriguez@wizeline.com>
2025-06-20 15:44:17 -0700

    ADD: Unit testing for DummyRepo

---
+++ internal/repository/dummy_currency_test.go
@@ -0,0 +1,40 @@
+package repository
+
+import (
+	"testing"
+
+	"github.com/erickrodriguez/currencygrpc/internal/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCurrentSearch(t *testing.T){
+	r := NewCurrencyRepo()
+	testCases := []struct{
+		name	string
+		want model.Currency
+		wantError bool
+		arg string
+	}{
+		{
+			name: "OK Found CountryCurrency",
+			want: model.Currency{ Base:"USD", CountryCode:"MXN", CurrencyName: "Mexican Peso",  Exchange: float32(19.00649) },
+			wantError: true,
+			arg: "MXN",
+		},
+				{
+			name: "ERROR Country not Supported",
+			want: model.Currency{},
+			wantError: false,
+			arg: "MXS",
+		},
+	}	
+
+	for _, tc := range testCases{
+		t.Run(tc.name, func(t *testing.T) {
+			got,err := r.Search(tc.arg)
+			assert.Equal(t,tc.want, got)
+			assert.Equal(t,tc.wantError, err)
+		})
+	}
+
+}
