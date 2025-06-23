5de23dda9b3f7e212c711eb499634d981a321e52
ErickRodriguezDev <erick.rodriguez@wizeline.com>
2025-06-23 12:53:39 -0700

    UPDATE | Client logic for Exchange Endpoint.

--- client/client.go
+++ client/client.go
@@ -13,25 +13,56 @@ import (
 
 var (
 	host = flag.String("port", "localhost:50051", "Port of the server is running.")
-	countryCode = flag.String("code", "NGN", "Country code to receive currency's converiton.")
+	countryCode = flag.String("code", "MXN", "Country code to receive currency's converiton.")
+	rpc = flag.String("rpc", "Converter", "Call specific RPC endpoint.")
+	amount = flag.Float64("amount", 10.00, "Amount to convert.")
 )
 
 func main(){
 	flag.Parse()
-	log.Printf("=== Client call -> %s", *host)
+	log.Printf("=== Client call -> %s\n", *host)
 
-	r, err := initClient()
-	if err != nil {
-		log.Fatalf("Could't create GRPC client: %v", err)
+	switch *rpc{
+	case "Converter":
+		ClientCallConverter(*countryCode)
+	case "Exchange":
+		a := float32(*amount)
+		ClientCallExchange(*countryCode,a)
+	default:
+		log.Println(" == No instructions were provided == ")
 	}
 
-	log.Printf("==== Converter Response:  %v", r)
-	log.Printf("==== Client Call completed ===")
+	log.Println("=== Client Call completed ===")
 }
 
-// Init GRPC client for Converter GRPC's server.
-func initClient()(*pb.ConverterResponse,error){
-	// create GRPC client-Connection.
+
+// Call  GRPC service Converter. 
+func ClientCallConverter (countryCode string){
+	log.Println("== CALL to CONVERTER RPC endpoint. ==")
+
+	conn,err := grpc.NewClient(*host, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("FAILED| Could not create GRPC Client: %v", err)
+	}
+	
+	// Create Client for Currency GRPC server. 
+	defer conn.Close()
+	c := pb.NewCurrencyClient(conn) 
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.Converter(ctx, &pb.ConverterRequest{CountryCode: &countryCode})
+	if err != nil {
+		log.Fatalf("FAILED| Couldn't complete RPC call: %v",err)
+	}
+
+	log.Printf("== Call Response: %v", r)
+}
+
+func ClientCallExchange(countryCode string, amount float32){
+	log.Println("== CALL to EXCHANGE RPC endpoint. ==")
+
 	conn,err := grpc.NewClient(*host, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("FAILED: Could not create GRPC Client: %v", err)
@@ -41,25 +72,13 @@ func initClient()(*pb.ConverterResponse,error){
 	defer conn.Close()
 	c := pb.NewCurrencyClient(conn) 
 
-	// Define context to be use when calling grpc Server.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	
-	// Call GRPC service Converter.
-	r, err := CallConverter(c, ctx)
+
+	r, err := c.Exchange(ctx, &pb.ExchangeRequest{CountryCode: &countryCode, Amount: &amount})
 	if err != nil {
-		log.Fatalf("FAILED:  %v", err)
+		log.Fatalf("FAILED| Couldn't complete RPC call : %v",err)
 	}
 
-	return r,nil
-}
-
-// Call  GRPC service Converter. 
-func CallConverter (c pb.CurrencyClient, ctx context.Context) (*pb.ConverterResponse, error){
-	r, err := c.Converter(ctx, &pb.ConverterRequest{CountryCode: countryCode})
-	if err != nil {
-		return nil, err
-	}
-
-	return r, nil
+	log.Printf(" == Call Response: %v", r)
 }
